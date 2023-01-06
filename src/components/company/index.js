c58fc0bb8256fbd21ff89535a38b605138a3167e
Gaylord Carrillo Caballero <gcarricaballero3@hotmail.com>
2023-01-06 11:50:12 -0600

    Add of a creation company's view

--- src/components/company/index.js
+++ src/components/company/index.js
@@ -4,22 +4,26 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import './__styles__/index.css';
+import { red } from '@mui/material/colors';
 
 const Company = () => {
     return (
-        <div className='main-wrapper'>
+        <div className='root'>
             <h1 className='title-main'>Registro de Empresas</h1>
             <div className='form-wrapper'>
                 <Box
                     component="form"
                     sx={{
-                            '& .MuiTextField-root': { m: 1, width: '35ch' },
+                            '& .MuiTextField-root': {marginTop: '10px', marginLeft: '20px' , },
+                            '& .MuiOutlinedInput-input': {width: '200px', height: '10px'},
                         }}
                         noValidate
                         autoComplete="off"
                 >
-                    <div>
-                        <TextField
+                    <div className='register-button'>
+                        <TextField 
+                            style={{color: "red"}}
+
                             id="outlined-required"
                             label="company"
                             placeholder="write company name"
@@ -42,8 +46,6 @@ const Company = () => {
                             label="adress"
                             placeholder="company_adress"
                         />
-                    </div>
-                    <div>
                         <TextField
                             id="standard-disabled"
                             label="email_contact"
@@ -57,6 +59,8 @@ const Company = () => {
                                 shrink: true,
                             }}
                         />
+                    </div>
+                    <div>
                         <TextField
                             id="filled-multiline-static"
                             label="Helper text"
