8fde77a05eeb26d6ed535bfaab41c04f06143b69
Gaylord Carrillo Caballero <gcarricaballero3@hotmail.com>
2023-01-10 15:01:25 -0500

    Implmentation of visit view

--- src/components/visit/index.js
+++ src/components/visit/index.js
@@ -1,18 +1,30 @@
 import React, {useState, state} from "react";
+import dayjs  from "dayjs";
 import Box from '@mui/material/Box'
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
-import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
+import Stack from '@mui/material/Stack';
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+
+
+const isWeekend = (date) => {
+    const day = date.day();
+  
+    return day === 0 || day === 6;
+};
+
 
 const Visit = () => {
-    const [fecha, setFecha]  = useState(new Date("2022", "06","22"));
-    const [profession, setProfession] = useState(null)
-    
-    const onChange = (event)=>{
-        setFecha(event)
-    }
+    // const [profession, setProfession] = useState(null);
+    const [value, setValue] = React.useState(dayjs('2022-04-15'));
+    const [value1, setValue1] = React.useState(dayjs('2018-01-01T00:00:00.000Z'));
+
         return (
             <div className= 'root' >
                 <h1 className= 'title-main' > Información de la Visita</h1>
@@ -22,22 +34,31 @@ const Visit = () => {
                             sx={{
                                     '& .MuiTextField-root': {marginTop: '10px', marginLeft: '20px' , },
                                     '& .MuiOutlinedInput-input': {width: '200px', height: '10px'},
-                                    '& .react-datepicker__input-container': {marginTop: '50px', marginBottom: '5px',marginLeft: '20px' , },
+                                    '& .react-datepicker__input-container': {marginTop: '50px', marginBottom: '5px',marginLeft: '20px' },
+                                    '& .MuiInputLabel-formControl': {width: '200px', height: '25px'},
 
                                 }}
                                 noValidate
                                 autoComplete="off"
                         >
                             <div className='register-button'>
-                                <DatePicker 
-                                    selected={fecha} 
-                                    onChange={(event) => onChange(event)}
-                                />
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <StaticDatePicker
+                                    label="Seleccione: fecha visita"
+                                    orientation="landscape"
+                                    openTo="day"
+                                    value={value}
+                                    onChange={(newValue) => {
+                                        setValue(newValue);
+                                    }}
+                                    renderInput={(params) => <TextField {...params}/>}
+                                    />
+                                 </LocalizationProvider>
                                 <TextField
                                     id="outlined-disabled"
                                     label="profesión"
                                     placeholder="profesion del responsable"
-                                    onChange={(event) => {setProfession(event.target.value)}}
+                                    // onChange={(event) => {setProfession(event.target.value)}}
                                 />
                                 <TextField
                                     id="outlined-disabled"
@@ -78,6 +99,34 @@ const Visit = () => {
                                         shrink: true,
                                     }}
                                 />
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <Stack spacing={3}>
+                                        <DesktopDateTimePicker
+                                            label="Día y Hora de Inicio"
+                                            value={value}
+                                            onChange={(newValue) => {
+                                                setValue(newValue);
+                                            }}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </Stack>
+                                </LocalizationProvider>
+                                
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <Stack spacing={3}>
+                                        <DesktopDateTimePicker
+                                            label="Día y Hora de Finalización"
+                                            value={value}
+                                            onChange={(newValue) => {
+                                                setValue(newValue);
+                                            }}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </Stack>
+                                </LocalizationProvider>
+
+
+
                             </div>
                             {/* <div>
                                 <TextField
