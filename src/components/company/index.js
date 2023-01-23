ccb1c80ae043b20adbab1511c3807c94d217026f
Gaylord Carrillo Caballero <gcarricaballero3@hotmail.com>
2023-01-23 10:56:06 -0500

    Add integration with endpoint to create company

--- src/components/company/index.js
+++ src/components/company/index.js
@@ -5,8 +5,33 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import './__styles__/index.css';
 import { red } from '@mui/material/colors';
+import axios from 'axios';
+import { createCompany } from '../../controllers/companies';
 
 const Company = () => {
+    const [name, setName] = React.useState(null);
+    const [city, setCity] = React.useState(null);
+    const [identification, setIdentification] = React.useState(null);
+    const [address, setAddress] = React.useState(null);
+    const [email, setEmail] = React.useState(null);
+    const [phone, setPhone] = React.useState(null);
+    const [helper, setHelper] = React.useState(null);
+
+    const handleCreateCompany = async() => {
+        const { succes, data, errors } = await createCompany({ name: name, city: city, 
+            identification: identification, address: address, email: email, phone: phone });
+        
+        if (succes) {
+            alert("TODO OK")
+        } else {
+            alert(" :(" + errors);
+        }
+    }
+
+    const handlerPhone = (event)=> {
+        setPhone(event.target.value)
+    }
+
     return (
         <div className='root'>
             <h1 className='title-main'>Registro de Empresas</h1>
@@ -23,54 +48,67 @@ const Company = () => {
                     <div className='register-button'>
                         <TextField 
                             style={{color: "red"}}
-
-                            id="outlined-required"
                             label="company"
                             placeholder="write company name"
+                            id={'name'}
+                            name={'name'}
+                            onChanege={(event) => setName(event.target.value)}
                         />
                         <TextField
-                            id="outlined-disabled"
                             label="city"
                             placeholder="Location"
+                            id={'city'}
+                            name={'city'}
+                            onChange={(event) => setCity(event.target.value)}
                         />
                         <TextField
-                            id="outlined-number"
                             label="identification_number"
                             type="number"
                             InputLabelProps={{
                                 shrink: true,
                             }}
+                            id={'identification'}
+                            name={'identification'}
+                            onChange={(event) => setIdentification(event.target.value)}
                         />
                         <TextField
-                            id="standard-required"
                             label="adress"
                             placeholder="company_adress"
+                            id={'address'}
+                            name={'address'}
+                            onChange={(event) => setAddress(event.target.value)}
                         />
                         <TextField
-                            id="standard-disabled"
                             label="email_contact"
                             placeholder="write email contact"
+                            id={'email'}
+                            name={'email'}
+                            onChange={(event) => setEmail(event.target.value)}
                         />
                         <TextField
-                            id="standard-number"
                             label="phone_number"
                             type="number"
                             InputLabelProps={{
                                 shrink: true,
                             }}
+                            id={'phone'}
+                            name={'phone'}
+                            onChange={handlerPhone}
                         />
                     </div>
                     <div>
                         <TextField
-                            id="filled-multiline-static"
                             label="Helper text"
                             multiline
                             rows={6}
                             placeholder="Add some aditional information"
+                            id={'helper'}
+                            name={'helper'}
+                            onChange={(event) => setHelper(event.target.value)}
                         />
                     </div>
                     <div className='sendButton'>
-                        <Button variant="contained" endIcon={<SendIcon />}>
+                        <Button variant="contained" endIcon={<SendIcon />} onClick={handleCreateCompany}>
                             Send
                         </Button>
                     </div>
---
+++ src/controllers/companies/index.js
@@ -0,0 +1,27 @@
+import httpRequest from '../../services/http_request';
+
+export const createCompany = (params) => {
+    try {
+        const response = httpRequest ({
+            method: "POST",
+            url: '/companies',
+            data: {
+                name: params.name,
+                city: params.city,
+                identification: params.identification,
+                adress: params.address,
+                email: params.email,
+                phone: params.phone
+            }
+        })
+        return {
+            succes: true,
+            data: response,
+        }
+    }catch (error) {
+        return {
+            succes: false,
+            error:error
+        }
+    }
+}
